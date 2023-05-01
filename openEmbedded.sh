11ce80803307c6d9735eaccff5c74431e800b3e5
Wlad <walfriesen@gmail.com>
2023-05-01 17:15:23 +0200

    new files for open embedded with Jetson Nano B01

---
+++ openEmbedded.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+start()
+{
+#usage: copy into conf folder and from there:
+# $ source openEmbedded && start
+	export PATH=$(pwd)/../../sources/bitbake/bin:$PATH
+	bitbake jetson-nano-devkit-emmc
+}
+
+populate_sdk()
+{
+	export PATH=$(pwd)/../../sources/bitbake/bin:$PATH
+	bitbake jetson-nano-devkit-emmc -c populate_sdk
+}
+
+u-boot()
+{
+	export PATH=$(pwd)/../../sources/bitbake/bin:$PATH
+	bitbake u-boot -c menuconfig
+}
+
+flash1-RPi()
+{
+	#https://blog.lazy-evaluation.net/posts/linux/bmaptool.html
+	time sudo bmaptool copy /run/media/waldi/4C47-78FE/raspberrypi3-64/core-image-sato-raspberrypi3-64-20221105144918.rootfs.wic.bz2 /dev/sdb
+}
+
+flash2-RPi()
+{
+	#https://blog.lazy-evaluation.net/posts/linux/bmaptool.html
+	time ( bzcat /run/media/waldi/4C47-78FE/raspberrypi3-64/core-image-sato-raspberrypi3-64-20221105144918.rootfs.wic.bz2 | sudo dd of=/dev/sdb bs=1M conv=fsync )
+}
